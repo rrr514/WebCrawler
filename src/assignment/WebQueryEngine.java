2c3a84ed5ff7e1e606a63213c41e3f72a7c72a69
Roy Yue <roy.yue05@gmail.com>
2023-12-01 20:25:14 -0600

    finish some of webquery

--- src/assignment/WebQueryEngine.java
+++ src/assignment/WebQueryEngine.java
@@ -1,9 +1,11 @@
 package assignment;
+
 import java.net.URL;
 import java.util.*;
 
 /**
- * A query engine which holds an underlying web index and can answer textual queries with a
+ * A query engine which holds an underlying web index and can answer textual
+ * queries with a
  * collection of relevant pages.
  *
  * TODO: Implement this!
@@ -13,7 +15,8 @@ public class WebQueryEngine {
     WebIndex webInd;
 
     /**
-     * Returns a WebQueryEngine that uses the given Index to constructe answers to queries.
+     * Returns a WebQueryEngine that uses the given Index to constructe answers to
+     * queries.
      *
      * @param index The WebIndex this WebQueryEngine should use.
      * @return A WebQueryEngine ready to be queried.
@@ -25,19 +28,123 @@ public class WebQueryEngine {
     }
 
     /**
-     * Returns a Collection of URLs (as Strings) of web pages satisfying the query expression.
+     * Returns a Collection of URLs (as Strings) of web pages satisfying the query
+     * expression.
      *
      * @param query A query expression.
      * @return A collection of web pages satisfying the query.
      */
     public Collection<Page> query(String query) {
-        query = query.toUpperCase();
-        LinkedList<Page> ret = new LinkedList<>();
-        Page p;
-        for(URL u: webInd.ind.get(query)){
-            p = new Page(u);
-            ret.add(p);
+        // query = query.toUpperCase();
+        // LinkedList<Page> ret = new LinkedList<>();
+        // Page p;
+        // for(URL u: webInd.ind.get(query)){
+        // p = new Page(u);
+        // ret.add(p);
+        // }
+        // return ret;
+        query = query.trim();
+        String tokens[] = query.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tokens.length-1; i++) {
+            sb.append(tokens[i]);
+            if (!isOperator(tokens[i].charAt(tokens[i].length()-1)) && 
+            !isOperator(tokens[i+1].charAt(0))){ 
+                sb.append(" ");
+            }
         }
-        return ret;
+        sb.append(tokens[tokens.length-1]);
+
+        StringBuilder sbTemp = new StringBuilder();
+
+        //TODO what if query is empty ^ go to top
+
+        sbTemp.append(sb.toString().charAt(0)); //assuming not empty
+        for (int i = 1; i < sb.toString().length(); i++) {
+            if (sb.toString().charAt(i-1) == ')' && sb.toString().charAt(i) == '(') {
+                sbTemp.append('&'); //add implicit & between opposing parentheses
+            }
+            sbTemp.append(""+sb.charAt(i));
+        }
+        sb = sbTemp;
+        ArrayList<String> infix = new ArrayList<String>();
+        StringBuilder sb2 = new StringBuilder();
+        boolean flag = false;
+        for (int i = 0; i < sb.length(); i++) {
+            //if it is a double quotation, flip flag
+            if (sb.charAt(i) == '"') flag = !flag;
+            //if it is a operator, add to infix array and reset word builder
+            if (isOperator(sb.charAt(i)) && sb.charAt(i) != '"'){ 
+                if (sb2.length() != 0){
+                    infix.add(sb2.toString());
+                    sb2.setLength(0);
+                }
+                infix.add("" + sb.charAt(i));
+            }
+            //if inside quotations, continue to next letter
+            if (flag && sb.charAt(i) == ' ') sb2.append(" ");
+            //if outside quotations, add an implicit 'and'
+            if (!flag && sb.charAt(i) == ' '){
+                if (sb2.length() != 0){
+                    infix.add(sb2.toString());
+                    sb2.setLength(0);
+                }
+                infix.add("&");
+            }
+            //if it is a letter, append to word builder
+            if (!isOperator(sb.charAt(i)) && sb.charAt(i) != ' ') sb2.append(""+sb.charAt(i));
+        }
+        //add whatever's left in the word builder
+        if (sb2.length() != 0){
+            infix.add(sb2.toString());
+        }
+
+        Stack<Character> ops = new Stack<>();
+        Queue<String> output = new LinkedList<String>();
+        char[] q = sb.toString().toCharArray();
+
+        for (int i = 0; i < q.length; i++) {
+            // TODO if word add to queue
+            switch (q[i]) {
+                case '(':
+                    ops.push(q[i]);
+                    break;
+                case ')':
+                    while (ops.peek() != '(') {
+                        output.add("" + ops.pop());
+                    }
+                    ops.pop();
+                    break;
+                
+            }
+
+        }
+
+        return null;
+
+        /*
+        pseudo solution for parsing queries
+        want to turn nonsense query into proper infix notation
+        turn all whitespaces into a single space if sandwhiched between two words, else delete space
+        iterate through query
+        if it is a operator (includes quotation marks), add to infix array
+        if it is a word, add to array
+        for spaces, if inside quotations, continue to next word
+        if outside quotations, add an implicit 'and'
+        use shunting yard to switch to RPN
+        */
+        
+    }
+
+    private boolean isOperator(char c){
+        switch(c){
+            case '(': return true;
+            case ')': return true;
+            case '!': return true;
+            case '&': return true;
+            case '|': return true;
+            case '"': return true;
+            default: return false;
+        }
     }
 }
--- test/assignment/WebCrawlerTest.java
+++ test/assignment/WebCrawlerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -33,17 +35,79 @@ public class WebCrawlerTest {
 
     @Test
     public void testWebCrawler() {
-        File f = new File("/Users/rrr/git-314h/314h-prog7-prog7group16/superspoof/www.superspoof.com/index.html");
-        try {
-            URL url = new URL("/Users/rrr/git-314h/314h-prog7-prog7group16/superspoof/www.superspoof.com/index.html");
-            File g = new File(url.toString());
-            assertTrue(f.exists());
-            assertTrue(g.exists());
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        String query = "(\" START          END\" words ) (\"oihwefihoiweh\") (word | word2) ";
+        query = query.trim();
+        String tokens[] = query.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tokens.length-1; i++) {
+            sb.append(tokens[i]);
+            if (!isOperator(tokens[i].charAt(tokens[i].length()-1)) && 
+            !isOperator(tokens[i+1].charAt(0))){ 
+                sb.append(" ");
+            }
         }
+        sb.append(tokens[tokens.length-1]);
 
+        StringBuilder sbTemp = new StringBuilder();
+
+        //TODO what if query is empty ^ go to top
+
+        sbTemp.append(sb.toString().charAt(0)); //assuming not empty
+        for (int i = 1; i < sb.toString().length(); i++) {
+            if (sb.toString().charAt(i-1) == ')' && sb.toString().charAt(i) == '(') {
+                sbTemp.append('&'); //add implicit & between opposing parentheses
+            }
+            sbTemp.append(""+sb.charAt(i));
+        }
+        sb = sbTemp;
+        ArrayList<String> infix = new ArrayList<String>();
+        StringBuilder sb2 = new StringBuilder();
+        boolean flag = false;
+        for (int i = 0; i < sb.length(); i++) {
+            //if it is a double quotation, flip flag
+            if (sb.charAt(i) == '"') flag = !flag;
+            //if it is a operator, add to infix array and reset word builder
+            if (isOperator(sb.charAt(i)) && sb.charAt(i) != '"'){ 
+                if (sb2.length() != 0){
+                    infix.add(sb2.toString());
+                    sb2.setLength(0);
+                }
+                infix.add("" + sb.charAt(i));
+            }
+            //if inside quotations, continue to next letter
+            if (flag && sb.charAt(i) == ' ') sb2.append(" ");
+            //if outside quotations, add an implicit 'and'
+            if (!flag && sb.charAt(i) == ' '){
+                if (sb2.length() != 0){
+                    infix.add(sb2.toString());
+                    sb2.setLength(0);
+                }
+                infix.add("&");
+            }
+            //if it is a letter, append to word builder
+            if (!isOperator(sb.charAt(i)) && sb.charAt(i) != ' ') sb2.append(""+sb.charAt(i));
+        }
+        //add whatever's left in the word builder
+        if (sb2.length() != 0){
+            infix.add(sb2.toString());
+        }
+        //debugging
+        System.out.println(sb);
+        System.out.println(sb2);
+        System.out.println("Infix Expression: " + (infix));
+
+    }
+
+    private boolean isOperator(char c){
+        switch(c){
+            case '(': return true;
+            case ')': return true;
+            case '!': return true;
+            case '&': return true;
+            case '|': return true;
+            case '"': return true;
+            default: return false;
+        }
     }
 
     @Test
